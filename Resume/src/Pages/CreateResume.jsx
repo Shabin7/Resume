751764a3ef22a763c50fee8075b700a4e62daa0d
Shabin Nicholas <163176715+Shabin7@users.noreply.github.com>
2024-07-30 22:46:14 +0530

    Update CreateResume.jsx

--- Resume/src/Pages/CreateResume.jsx
+++ Resume/src/Pages/CreateResume.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 
+
 const CreateResume = () => {
     const [form, setForm] = useState({
         name: '',
